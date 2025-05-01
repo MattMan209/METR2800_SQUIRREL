e134c2841621c9d5b292483673024f33f64df015
Riley2633 <151729771+Riley2633@users.noreply.github.com>
2025-05-01 13:51:02 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ Servo servo1;
 // declarations here
 unsigned int SensorReadingCound;
 const uint8_t SensorCount = 4;
+const uint8_t posVoltage = 8;
 uint16_t sensorReadings[SensorCount]; //main sensor reading values
 uint16_t sensorCalibratedValue[SensorCount];
 uint16_t sensorValuesArray[SensorCount][3];
 uint32_t servo1pos = 0;
 
-// put function declarations here:
 bool edgeEstimate(uint16_t sensorReadings[4]);
 //uint16_t ValueAddArray(uint16_t sensorReadings[4],uint16_t sensorValuesArray[4][3]);
 int32_t edgeCalibration(bool Run, bool Use);
@@ -38,7 +38,6 @@ void setup() {
   qtr.setDimmingLevel(0);
   qtr.setEmitterPin(52);
 
-
   //Set up servo's
   servo1.attach(12);
 
@@ -53,12 +52,12 @@ void loop() {
   //run edge estimate 100 times
   edgeEstimate(sensorReadings);
 
-  servoRun(0,180,30);
+  //servoRun(0,180,30);
 
-  if (SensorReadingCound>100){
-    delay(1000);
-    SensorReadingCound = 0;
-  } 
+  // if (SensorReadingCound>100){
+  //   delay(1000);
+  //   SensorReadingCound = 0;
+  // } 
   delay(10);
 
 }
@@ -94,7 +93,6 @@ bool edgeEstimate(uint16_t sensorReadings[4]) {
 
 int32_t edgeCalibration(bool Run, bool Use){
   if (!Run) return 0;
-  
   // define variables
   uint32_t readingSum[SensorCount] = {0};
   uint32_t averageReading[SensorCount];
@@ -111,45 +109,34 @@ int32_t edgeCalibration(bool Run, bool Use){
   }
   Serial.println();
 
-
-
   // read 100 values and add each reading to variable
   for (uint8_t j = 0; j < calibrationReadings; j++){
     qtr.read(sensorReadings);
     for (uint8_t k = 0; k < SensorCount; k++) {
       readingSum[k] += sensorReadings[k];
-      //Serial.print(sensorReadings[k]);
-      //Serial.print('\t');
+
     }
     delay(20);
   }
-  
-
   // divide avg value of each by the number of readings
   Serial.println("Calibration values:");
   for (uint8_t l = 0; l < SensorCount; l++) {
-    //Serial.print(readingSum[l]);
-    //Serial.print('\t');
     averageReading[l] = (readingSum[l]/calibrationReadings);  
     
     Serial.print(averageReading[l]);
     Serial.print('\t');
-    if (Use)
-    {
-      maxVals[l] = averageReading[l] + 8;
-    }
-    
+    if (Use) {maxVals[l] = averageReading[l] + posVoltage;}
   }
   delay(1000);
   Serial.println();
   Serial.println("Program Start In:");
   delay(1000);
+
   for (int8_t i = 3; i >= 0; i--){
     //Serial.println("Check");
     Serial.print(i);
     Serial.print('\t');
-    delay(800);
-  }
+    delay(800); }
   Serial.println();
   return 0;
 }
@@ -186,3 +173,4 @@ int32_t servoRun(int32_t startDegrees, int32_t endDegrees, int32_t delayTime){
     delay(delayTime);                       // waits 15ms for the servo to reach the position
   }
 }
+
